4f68bbc61785fcb17ecf28a86b6c4caed53a0d17
Andrew Johnson <ajohnson.uva@gmail.com>
2018-11-26 15:35:17 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -5,7 +5,7 @@ def my_find(collection)
   i = 0
   while i < collection.size
     # binding.pry
-    return yield(collection[i]) if yield(collection[i])
+    return yield(collection.to_a[i]) if yield(collection.to_a[i])
     i += 1
   end
 end
